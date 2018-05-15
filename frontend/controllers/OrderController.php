e7fa163a08d37e463861928f4b39fbd948381488
maxue1985 <maxue_@126.com>
2018-05-15 17:55:12 +0800

    22
    22

--- frontend/controllers/OrderController.php
+++ frontend/controllers/OrderController.php
@@ -9,6 +9,9 @@ use yii\web\Controller;
 class OrderController extends Controller
 {
 	public $layout = false;
+	public function actionIndex(){
+		return $this->render('index');
+	}
 	public function actionCheck()
     {
         return $this->render('check');
