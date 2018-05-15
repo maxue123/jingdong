8402a9e87c82efc9eb9c0589e3f5a07c12159449
maxue1985 <maxue_@126.com>
2018-05-15 18:58:41 +0800

    1
    1

--- common/config/main.php
+++ common/config/main.php
@@ -1,6 +1,18 @@
 <?php
-return [
-    'aliases' => [
+// return [
+//     'aliases' => [
+//         '@bower' => '@vendor/bower-asset',
+//         '@npm'   => '@vendor/npm-asset',
+//     ],
+//     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+//     'components' => [
+//         'cache' => [
+//             'class' => 'yii\caching\FileCache',
+//         ],
+//     ],
+// ];
+return [ 
+'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
@@ -10,4 +22,35 @@ return [
             'class' => 'yii\caching\FileCache',
         ],
     ],
+'modules' => [ 
+    'gii' => [ 
+        'class' => 'yii\gii\Module', 
+        'allowedIPs' => ['::1','127.0.0.1','192.168.0.222'], //只允许本地访问gii 
+        'generators'=> [ 
+            'module'=> [ 
+            'class' => 'yii\gii\generators\module\Generator', 
+            'templates'=> [ 
+                'backend'=>'@common/gii/generators/module/default' 
+                ] 
+            ], 
+            'model'=> [ 
+                'class' => 'yii\gii\generators\model\Generator', 
+                'baseClass'=> 'base\BaseActiveRecord', 
+                'ns'=> 'common\models', 
+                'templates'=> [ 
+                'common'=>'@common/gii/generators/model/default', 
+                'backend'=>'@common/gii/generators/model/backend' 
+                ] 
+            ], 
+            'crud'=> [ 
+                'class' => 'yii\gii\generators\crud\Generator', 
+                'templates'=> [ 
+                    'backend'=>'@common/gii/generators/crud/default' 
+                ], 
+                'baseControllerClass' => 'BaseBackendController', 
+                'messageCategory'=> 'backend' 
+            ] 
+        ] 
+    ] 
+    ] 
 ];
