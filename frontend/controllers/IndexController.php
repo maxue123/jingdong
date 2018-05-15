140b70685558ba12e7e71d6cdb60b78cc5eb3f22
maxue1985 <maxue_@126.com>
2018-05-15 18:18:19 +0800

    布局
    布局

--- frontend/controllers/IndexController.php
+++ frontend/controllers/IndexController.php
@@ -8,7 +8,7 @@ use yii\web\Controller;
  */
 class IndexController extends Controller
 {
-	public $layout = false;
+	public $layout = "layouts1";
     public function actionIndex()
     {
         return $this->render('index');
